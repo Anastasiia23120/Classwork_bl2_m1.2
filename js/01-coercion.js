31f13ddc8ee4920103e8e4f14fc99cb310ddde15
Anastasiia Medved <nastya0662793393@gmail.com>
2024-11-23 17:21:05 +0100

    js 01-02

---
+++ js/01-coercion.js
@@ -0,0 +1,18 @@
+// console.log(Number('5'));
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(Number(null));
+// console.log(Number(undefined));
+// console.log(Number('Jacob'));
+// console.log(Number('25px'));
+
+// const result = 10 + 20;
+
+// console.log(+'20');
+
+// console.log('5' * 2);
+// console.log('10' - 5);
+// console.log(5 + true);
+// console.log(5 - true);
+
+// console.log(2 + '5');
