fc698d8ffc13295b352e78ea1fb93b907516e05f
Anastasiia Medved <nastya0662793393@gmail.com>
2024-11-24 00:05:06 +0100

    html+js1-4

---
+++ js/03-numbers.js
@@ -0,0 +1,37 @@
+// const str = '10';
+// const num = Number(str);
+// console.log(str);
+// console.log(num);
+
+// const userInput = Number(prompt(`Enter your number`));
+// console.log(userInput);
+
+// const elementWidth = '50px';
+// const numericalWidth = Number.parseInt(elementWidth);
+// console.log('elementWisth:', elementWidth);
+// console.log('numericalWidthL:', numericalWidth);
+
+// const elementHeight = '200.74px';
+// const numericalHeight = Number.parseFloat(elementHeight);
+// console.log('elementHeight:', elementHeight);
+// console.log('numericalHeigh:', numericalHeight);
+
+// const numA = Number(prompt('Enter first number'));
+// const numB = Number(prompt('Enter second number'));
+// const result = numA + numB;
+// console.log(result);
+
+// let value = 27.3;
+// console.log(value);
+// value = Math.round(value);
+// console.log(value);
+
+// let value = 27.9;
+// console.log(value);
+// value = Math.floor(value);
+// console.log(value);
+
+// let value = -27.3;
+// console.log(value);
+// value = Math.ceil(value);
+// console.log(value);
---
+++ js/04-functions.js
@@ -0,0 +1,43 @@
+// function greet(firstName, lastName) {
+//   console.log(`Hello ${firstName} ${lastName}!`);
+// }
+
+// greet('Anastasiia', 'Medved');
+
+// greet('Diana');
+// greet();
+//////////////////////////////////////////////////////
+// function sum(numA, numB) {
+//   const result = numA + numB;
+//   return result;
+// }
+// console.log(sum(10, 20));
+/////////////////////////////////////////////////////
+// function getRectArea(sideA, sideB) {
+//   const rectArea = sideA * sideB;
+//   return rectArea;
+// }
+
+// console.log(getRectArea(8, 11));
+// console.log(getRectArea(18, 11));
+// console.log(getRectArea(8, 21));
+/////////////////////////////////////////////////////
+// function calcBMI(weight, height) {
+//   weight = Number(weight);
+//   height = Number(height);
+//   console.log(weight);
+//   console.log(height);
+// }
+// const bmi = calcBMI('88.3', '1.75');
+/////////////////////////////////////////////////////
+// function calcBMI(weight, height) {
+//   weight = Number(weight);
+//   height = Number(height);
+
+//   const resultBmi = weight / height ** 2;
+
+//   return Number(resultBmi.toFixed(1));
+// }
+
+// const bmi = calcBMI('88.3', '1.75');
+// console.log(bmi);
